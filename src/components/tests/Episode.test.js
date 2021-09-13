94a28bd49ac94bc0373c31447797d971c204c3e8
Joe Escobedo <joe_escob1023@live.com>
2021-09-13 15:30:06 -0700

    completed all the tests that I could

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,32 +1,53 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Episode from './../Episode';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Episode from "./../Episode";
 
 const testEpisode = {
-    id:1,
-    name: "",
-    image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
-    season: 1,
-    number: 1,
-    summary: "",
-    runtime: 1
-}
+  id: 1,
+  name: "",
+  image:
+    "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+  season: 1,
+  number: 1,
+  summary: "This is a summary",
+  runtime: 1,
+};
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
-}
+  //Add in approprate test data structure here.
+  id: 1,
+  name: "",
+  image: null,
+  season: 1,
+  number: 1,
+  summary: "This is a summary",
+  runtime: 1,
+};
 
 test("renders without error", () => {
-
+  render(<Episode episode={testEpisode} />);
 });
 
-test("renders the summury test passed as prop", ()=>{
-    
+test("renders the summary test passed as prop", () => {
+  render(<Episode episode={testEpisode} />);
+
+  const testSummary = screen.getByText("This is a summary");
+  console.log(testEpisode.summary);
+  console.log(testSummary);
+
+  expect(testSummary).toBeInTheDocument();
+  expect(testSummary).not.toBeNull();
+  expect(testSummary).toHaveTextContent(/this is a summary/i);
 });
 
-test("renders default image when image is not defined", ()=>{
-    
-})
+test("renders default image when image is not defined", () => {
+  render(<Episode episode={testEpisodeWithoutImage} />);
+  console.log(testEpisodeWithoutImage.image);
+
+  const image = screen.getByRole("img");
+  console.log(image);
+  expect(image).toHaveAttribute("alt", "./stranger_things.png");
+});
 
 //Tasks
 //1. Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
